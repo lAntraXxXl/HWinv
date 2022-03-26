ccb7010af2b6b72d85ab2416b9af245b77989b87
lAntraXxXl <isai.iturriaga.garcia@hotmail.com>
2022-03-26 12:19:24 -0600

    Updates

---
+++ precache-manifest.8a2c6a9b905c3c2f6e6f706f8fb693fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e975848e8e7edbaa1e9",
+    "url": "/HWinv/static/css/main.3f392fe1.chunk.css"
+  },
+  {
+    "revision": "7e975848e8e7edbaa1e9",
+    "url": "/HWinv/static/js/main.7e975848.chunk.js"
+  },
+  {
+    "revision": "615e02e421085e246fee",
+    "url": "/HWinv/static/js/1.615e02e4.chunk.js"
+  },
+  {
+    "revision": "9b6e236f39824561ebaa",
+    "url": "/HWinv/static/js/runtime~main.9b6e236f.js"
+  },
+  {
+    "revision": "3699e1751a3905b10fe1227503381f30",
+    "url": "/HWinv/static/media/bg.3699e175.svg"
+  },
+  {
+    "revision": "23a9bcc5043a69a25c41c4665bd373a7",
+    "url": "/HWinv/index.html"
+  }
+];
